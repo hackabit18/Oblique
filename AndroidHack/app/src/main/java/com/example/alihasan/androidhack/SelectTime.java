10a327ebf165de47ccf641c185b0761a63df57a8
Zyro9922 <alihasan9922@gmail.com>
2018-10-27 01:00:40 +0530

    Added send feature

--- AndroidHack/app/src/main/java/com/example/alihasan/androidhack/SelectTime.java
+++ AndroidHack/app/src/main/java/com/example/alihasan/androidhack/SelectTime.java
@@ -4,6 +4,7 @@ import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +12,15 @@ import android.widget.NumberPicker;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.io.UnsupportedEncodingException;
 import java.util.Calendar;
 
 public class SelectTime extends AppCompatActivity {
@@ -31,6 +41,15 @@ public class SelectTime extends AppCompatActivity {
 
     Button next;
 
+    /**
+     * MQTT
+     */
+    MqttConnectOptions options;
+
+    final static private String TAG = "ALI";
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +63,14 @@ public class SelectTime extends AppCompatActivity {
         ct2 = findViewById(R.id.ct2);
         ct3 = findViewById(R.id.ct3);
         ct4 = findViewById(R.id.ct4);
+
+        /**
+         * MQTT
+         */
+        options = new MqttConnectOptions();
+        options.setMqttVersion(MqttConnectOptions.MQTT_VERSION_3_1);
+        options.setUserName("aakashk_kvjp58");
+        options.setPassword("46f406136c5e4e5f874e283f95ed3dfc".toCharArray());
 
 
         /**
@@ -159,35 +186,43 @@ public class SelectTime extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                /**
+                 * MQTT
+                 */
+                options = new MqttConnectOptions();
+                options.setMqttVersion(MqttConnectOptions.MQTT_VERSION_3_1);
+                options.setUserName("aakashk_kvjp58");
+                options.setPassword("46f406136c5e4e5f874e283f95ed3dfc".toCharArray());
+
                 int n = np.getValue();
 
 //                TESTING INTENT
-
-                Intent i = new Intent(SelectTime.this,DateActivity.class);
-                i.putExtra("Value",n);
-                startActivity(i);
+//
+//                Intent i = new Intent(SelectTime.this,DateActivity.class);
+//                i.putExtra("Value",n);
+//                startActivity(i);
 
 
                 switch(n)
                 {
                     case 1:
                         Toast.makeText(SelectTime.this,ct1.getText().toString(),Toast.LENGTH_LONG).show();
-                        i.putExtra("ChooseTime1",ct1.getText().toString());
+//                        i.putExtra("ChooseTime1",ct1.getText().toString());
                         break;
                     case 2:
-                        i.putExtra("ChooseTime1",ct1.getText().toString());
-                        i.putExtra("ChooseTime2",ct2.getText().toString());
+//                        i.putExtra("ChooseTime1",ct1.getText().toString());
+//                        i.putExtra("ChooseTime2",ct2.getText().toString());
                         break;
                     case 3:
-                        i.putExtra("ChooseTime1",ct1.getText().toString());
-                        i.putExtra("ChooseTime2",ct2.getText().toString());
-                        i.putExtra("ChooseTime3",ct3.getText().toString());
+//                        i.putExtra("ChooseTime1",ct1.getText().toString());
+//                        i.putExtra("ChooseTime2",ct2.getText().toString());
+//                        i.putExtra("ChooseTime3",ct3.getText().toString());
                         break;
                     case 4:
-                        i.putExtra("ChooseTime1",ct1.getText().toString());
-                        i.putExtra("ChooseTime2",ct2.getText().toString());
-                        i.putExtra("ChooseTime3",ct3.getText().toString());
-                        i.putExtra("ChooseTime4",ct4.getText().toString());
+//                        i.putExtra("ChooseTime1",ct1.getText().toString());
+//                        i.putExtra("ChooseTime2",ct2.getText().toString());
+//                        i.putExtra("ChooseTime3",ct3.getText().toString());
+//                        i.putExtra("ChooseTime4",ct4.getText().toString());
                         break;
                     default:
                         Toast.makeText(SelectTime.this,"0 selected",Toast.LENGTH_LONG).show();
@@ -225,6 +260,101 @@ public class SelectTime extends AppCompatActivity {
 
         timePickerDialog.show();
 
+    }
+
+    private void SendMessagetoAakash(final String str){
+
+        String clientId = MqttClient.generateClientId();
+        final MqttAndroidClient client =
+                new MqttAndroidClient(getApplicationContext(), "tcp://io.adafruit.com:1883",
+                        clientId);
+
+        try {
+            IMqttToken token = client.connect(options);
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    Log.d(TAG, "onSuccess");
+                    Toast toast = Toast.makeText(getApplicationContext(), "Connection Done", Toast.LENGTH_SHORT);
+                    toast.show();
+
+                    //Subscribing
+                    final String topic = "aakashk_kvjp58/f";
+                    int qos = 1;
+                    try {
+                        IMqttToken subToken = client.subscribe(topic, qos);
+                        subToken.setActionCallback(new IMqttActionListener() {
+                            @Override
+                            public void onSuccess(IMqttToken asyncActionToken) {
+                                // The message was published
+                                Toast.makeText(SelectTime.this, "Successfully subscribed to: " + topic, Toast.LENGTH_SHORT).show();
+
+                                //sending the message
+
+                                String user_message = str;
+                                String topic = "aakashk_kvjp58/f/switch1";
+                                String payload = user_message;
+                                byte[] encodedPayload = new byte[0];
+                                try {
+                                    encodedPayload = payload.getBytes("UTF-8");
+                                    MqttMessage message = new MqttMessage(encodedPayload);
+                                    client.publish(topic, message);
+                                    Toast.makeText(SelectTime.this, "Message Sent", Toast.LENGTH_SHORT).show();
+
+                                } catch (UnsupportedEncodingException | MqttException e) {
+                                    e.printStackTrace();
+                                }
+
+                                // disconnecting after task is done
+                                try {
+                                    IMqttToken disconToken = client.disconnect();
+                                    disconToken.setActionCallback(new IMqttActionListener() {
+                                        @Override
+                                        public void onSuccess(IMqttToken asyncActionToken) {
+                                            // we are now successfully disconnected
+                                        }
+
+                                        @Override
+                                        public void onFailure(IMqttToken asyncActionToken,
+                                                              Throwable exception) {
+                                            // something went wrong, but probably we are disconnected anyway
+                                        }
+                                    });
+                                } catch (MqttException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+
+                            @Override
+                            public void onFailure(IMqttToken asyncActionToken,
+                                                  Throwable exception) {
+                                // The subscription could not be performed, maybe the user was not
+                                // authorized to subscribe on the specified topic e.g. using wildcards
+                                Toast.makeText(SelectTime.this, "Couldn't subscribe to: " + topic, Toast.LENGTH_SHORT).show();
+
+
+                            }
+                        });
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Log.d(TAG, "onFailure");
+                    Toast toast1 = Toast.makeText(getApplicationContext(), "failed", Toast.LENGTH_SHORT);
+                    toast1.show();
+
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 
     }
