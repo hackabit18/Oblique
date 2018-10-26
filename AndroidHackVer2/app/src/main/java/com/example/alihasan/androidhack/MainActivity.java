159ac58ba9bf426cbcfc79bfdd1bec78036f4ef9
Divyanshuk <divyanshuk34@gmail.com>
2018-10-27 02:44:03 +0530

    AndroidHack2

---
+++ AndroidHackVer2/app/src/main/java/com/example/alihasan/androidhack/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.alihasan.androidhack;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    NumberPicker np;
+    EditText Phone1;
+    Button next;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        np = findViewById(R.id.np);
+        Phone1 = findViewById(R.id.Phone1);
+        next = findViewById(R.id.next);
+
+        np.setMinValue(0);
+        np.setMaxValue(1);
+
+        np.setWrapSelectorWheel(true);
+
+        np.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+
+                switch(newVal)
+                {
+                    case 1:
+                        Phone1.setVisibility(View.VISIBLE);
+                    default:
+                        Toast.makeText(MainActivity.this, "Only "+newVal+" Participant", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,SelectTime.class);
+                startActivity(i);
+
+                /**
+                 * ADD NUMBER
+                 * IN SHARED PREFERENCE
+                 */
+
+            }
+        });
+
+    }
+    }
---
+++ AndroidHackVer2/app/src/main/java/com/example/alihasan/androidhack/ReceiverService.java
@@ -0,0 +1,146 @@
+package com.example.alihasan.androidhack;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class ReceiverService extends Service {
+
+    final static private String TAG = "ALI";
+    private Handler handler = new Handler();
+    private Runnable runnable;
+    MqttConnectOptions options = new MqttConnectOptions();
+
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Log.d(TAG, "onStartCommand");
+
+
+        options.setMqttVersion(MqttConnectOptions.MQTT_VERSION_3_1);
+        options.setUserName("aakashk_kvjp58");
+        options.setPassword("46f406136c5e4e5f874e283f95ed3dfc".toCharArray());
+        String clientId = MqttClient.generateClientId();
+        final MqttAndroidClient client =
+                new MqttAndroidClient(getApplicationContext(), "tcp://io.adafruit.com:1883",
+                        clientId);
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+
+
+
+
+                try {
+                    IMqttToken token = client.connect(options);
+                    token.setActionCallback(new IMqttActionListener() {
+                        @Override
+                        public void onSuccess(IMqttToken asyncActionToken) {
+                            // We are connected
+                            Log.d(TAG, "onSuccess");
+//                    Toast toast = Toast.makeText(getApplicationContext(), "Connection Done", Toast.LENGTH_SHORT);
+//                    toast.show();
+
+                            //Subscribing
+                            final String topic = "aakashk_kvjp58/f/switch1";
+                            int qos = 1;
+                            try {
+                                IMqttToken subToken = client.subscribe(topic, qos);
+                                subToken.setActionCallback(new IMqttActionListener() {
+                                    @Override
+                                    public void onSuccess(IMqttToken asyncActionToken) {
+
+                                    }
+                                    @Override
+                                    public void onFailure(IMqttToken asyncActionToken,
+                                                          Throwable exception) {
+                                        // The subscription could not be performed, maybe the user was not
+                                        // authorized to subscribe on the specified topic e.g. using wildcards
+//                                Toast.makeText(MqttReceiver.this, "Couldn't subscribe to: " + topic, Toast.LENGTH_SHORT).show();
+
+
+                                    }
+                                });
+                            } catch (MqttException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                            // Something went wrong e.g. connection timeout or firewall problems
+                            Log.d(TAG, "onFailure");
+//                    Toast toast1 = Toast.makeText(getApplicationContext(), "failed", Toast.LENGTH_SHORT);
+//                    toast1.show();
+
+
+                        }
+                    });
+
+
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+                handler.postDelayed(runnable, 2000);
+
+            }
+        };
+        handler.post(runnable);
+
+        client.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+                Toast.makeText(getApplicationContext(), "LOST", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+
+                Toast.makeText(getApplicationContext(), message.toString(), Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "received");
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+                Toast.makeText(getApplicationContext(), "completed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        return START_STICKY;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+//    @Override
+//    public void onDestroy() {
+//        Log.d(TAG, "inDestroy");
+//        Intent i = new Intent(this, ReceiverBroadcast.class);
+//        sendBroadcast(i);
+//
+//    }
+
+
+}
