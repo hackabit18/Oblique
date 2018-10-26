a186605818726d20439a5212318cdf13bbccafe3
Divyanshuk <divyanshuk34@gmail.com>
2018-10-27 02:20:01 +0530

    made Constructor

--- AndroidHack/app/src/main/java/com/example/alihasan/androidhack/SharedPrefClass.java
+++ AndroidHack/app/src/main/java/com/example/alihasan/androidhack/SharedPrefClass.java
@@ -1,5 +1,19 @@
 package com.example.alihasan.androidhack;
 
+import android.content.Context;
+
 public class SharedPrefClass {
-    
+
+    Context context;
+    String timings;
+
+    public SharedPrefClass(Context context1, String timings1){
+
+        context = context1;
+        timings = timings1;
+
+    }
+
+
+
 }
